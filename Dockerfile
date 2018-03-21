ac7aabee9c9be6e258c745210385cdba7c5804be
William Rory Kronmiller <wrk961@gmail.com>
2018-03-21 07:22:38 -0400

    Move some ubuntu setup scripts into dockerfile

--- Dockerfile
+++ Dockerfile
@@ -19,8 +19,73 @@ COPY . $WORKDIR
 CMD ["bash"]
 
 # Setup the docker container.
+ENV FB_VERSION="2017.05.22.00"
+ENV ZSTD_VERSION="1.1.1"
+
 WORKDIR $WORKDIR
+
+RUN apt update
+
+RUN apt install --yes \
+    autoconf \
+    autoconf-archive \
+    automake \
+    binutils-dev \
+    bison \
+    clang-format-3.9 \
+    cmake \
+    flex \
+    g++ \
+    git \
+    gperf \
+    libboost-all-dev \
+    libcap-dev \
+    libdouble-conversion-dev \
+    libevent-dev \
+    libgflags-dev \
+    libgoogle-glog-dev \
+    libjemalloc-dev \
+    libkrb5-dev \
+    liblz4-dev \
+    liblzma-dev \
+    libnuma-dev \
+    libsasl2-dev \
+    libsnappy-dev \
+    libssl-dev \
+    libtool \
+    make \
+    pkg-config \
+    scons \
+    wget \
+    zip \
+    zlib1g-dev
+
+RUN mkdir -pv /usr/local/facebook-${FB_VERSION}
+RUN ln -sfT /usr/local/facebook-${FB_VERSION} /usr/local/facebook
+
+ENV LDFLAGS="-L/usr/local/facebook/lib -Wl,-rpath=/usr/local/facebook/lib"
+ENV CPPFLAGS="-I/usr/local/facebook/include"
+
+WORKDIR /tmp
+
+wget -O /tmp/folly-${FB_VERSION}.tar.gz https://github.com/facebook/folly/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/wangle-${FB_VERSION}.tar.gz https://github.com/facebook/wangle/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/fbthrift-${FB_VERSION}.tar.gz https://github.com/facebook/fbthrift/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/proxygen-${FB_VERSION}.tar.gz https://github.com/facebook/proxygen/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/mstch-master.tar.gz https://github.com/no1msd/mstch/archive/master.tar.gz
+wget -O /tmp/zstd-${ZSTD_VERSION}.tar.gz https://github.com/facebook/zstd/archive/v${ZSTD_VERSION}.tar.gz
+
+tar xzvf folly-${FB_VERSION}.tar.gz
+tar xzvf wangle-${FB_VERSION}.tar.gz
+tar xzvf fbthrift-${FB_VERSION}.tar.gz
+tar xzvf proxygen-${FB_VERSION}.tar.gz
+tar xzvf mstch-master.tar.gz
+tar xzvf zstd-${ZSTD_VERSION}.tar.gz
+
+
 RUN $WORKDIR/setup_ubuntu.sh
+
+WORKDIR $WORKDIR
 
 # Create a build directory.
 RUN mkdir $WORKDIR/build
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,47 +1,8 @@
 #!/bin/bash
 set -e
 
-FB_VERSION="2017.05.22.00"
-ZSTD_VERSION="1.1.1"
-
 echo "This script configures ubuntu with everything needed to run beringei."
 echo "It requires that you run it as root. sudo works great for that."
-
-apt update
-
-apt install --yes \
-    autoconf \
-    autoconf-archive \
-    automake \
-    binutils-dev \
-    bison \
-    clang-format-3.9 \
-    cmake \
-    flex \
-    g++ \
-    git \
-    gperf \
-    libboost-all-dev \
-    libcap-dev \
-    libdouble-conversion-dev \
-    libevent-dev \
-    libgflags-dev \
-    libgoogle-glog-dev \
-    libjemalloc-dev \
-    libkrb5-dev \
-    liblz4-dev \
-    liblzma-dev \
-    libnuma-dev \
-    libsasl2-dev \
-    libsnappy-dev \
-    libssl-dev \
-    libtool \
-    make \
-    pkg-config \
-    scons \
-    wget \
-    zip \
-    zlib1g-dev
 
 ready_destdir() {
         if [[ -e ${2} ]]; then
@@ -49,28 +10,6 @@ ready_destdir() {
                 mv -v "$2" "$2.bak.$(date +%Y-%m-%d)"
         fi
 }
-
-mkdir -pv /usr/local/facebook-${FB_VERSION}
-ln -sfT /usr/local/facebook-${FB_VERSION} /usr/local/facebook
-
-export LDFLAGS="-L/usr/local/facebook/lib -Wl,-rpath=/usr/local/facebook/lib"
-export CPPFLAGS="-I/usr/local/facebook/include"
-
-cd /tmp
-
-wget -O /tmp/folly-${FB_VERSION}.tar.gz https://github.com/facebook/folly/archive/v${FB_VERSION}.tar.gz
-wget -O /tmp/wangle-${FB_VERSION}.tar.gz https://github.com/facebook/wangle/archive/v${FB_VERSION}.tar.gz
-wget -O /tmp/fbthrift-${FB_VERSION}.tar.gz https://github.com/facebook/fbthrift/archive/v${FB_VERSION}.tar.gz
-wget -O /tmp/proxygen-${FB_VERSION}.tar.gz https://github.com/facebook/proxygen/archive/v${FB_VERSION}.tar.gz
-wget -O /tmp/mstch-master.tar.gz https://github.com/no1msd/mstch/archive/master.tar.gz
-wget -O /tmp/zstd-${ZSTD_VERSION}.tar.gz https://github.com/facebook/zstd/archive/v${ZSTD_VERSION}.tar.gz
-
-tar xzvf folly-${FB_VERSION}.tar.gz
-tar xzvf wangle-${FB_VERSION}.tar.gz
-tar xzvf fbthrift-${FB_VERSION}.tar.gz
-tar xzvf proxygen-${FB_VERSION}.tar.gz
-tar xzvf mstch-master.tar.gz
-tar xzvf zstd-${ZSTD_VERSION}.tar.gz
 
 pushd mstch-master
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/facebook-${FB_VERSION} .
